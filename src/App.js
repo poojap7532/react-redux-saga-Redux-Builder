9dcaf1fefb42b78722a98d40f0539093f9dac8f4
poojap7532 <130437269+poojap7532@users.noreply.github.com>
2024-01-03 17:47:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { GET_PRODUCT_PROGRESS,POST_PRODUCT_PROGRESS,DELETE_PRODUCT_PROGRESS,PATCH_PRODUCT_PROGRESS } from "./redux-saga/admin/action/action";
+import Data from "./components/Data";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type:GET_PRODUCT_PROGRESS,
+      POST_PRODUCT_PROGRESS,
+      DELETE_PRODUCT_PROGRESS,
+      PATCH_PRODUCT_PROGRESS,});
+  }, []);
+
+  return (
+    <div className="App">
+      redux saga
+      <Data />
+    </div>
+  );
+}
+export default App;
